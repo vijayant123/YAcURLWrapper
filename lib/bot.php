dc8d91fc9750f15dcf91d501523631901e6391bd
Vijayant <vijayant.saini123@gmail.com>
2016-01-17 13:47:27 +0530

    bot and response class

--- lib/bot.php
+++ lib/bot.php
@@ -1,16 +1,46 @@
 <?php
 
+/**
+ * bot class for YAcURLWrapper
+ * @author vijayant
+ * @usage accepts a request object and returns a response object
+ * Note: the bot has been implemented as a singleton!
+ **/
+
 namespace bot {
 
+    require_once "request.php";
+    require_once "resposne.php";
+
     class bot {
-        private $status;
-        private $content;
+        private $ch;
+        private $response;
+        private $instance;
+
+        private function __construct(){}
+
+        public static function getInstance(){
+            if(isset($instance))
+                return $instance;
+            else
+                return $instance = new \bot\bot;
+        }
+
+        public function execute(\bot\request $request){
+            $this->ch = curl_init();
+            $this->response = new \bot\response;
+
+            curl_setopt_array($this->ch, $request->getRequest());
+
+            $this->response->setContent(curl_exec($this->ch));
+            $this->response->setInfo(curl_getinfo($this->ch));
+
+            curl_close($this->ch);
+
+            return $this->response;
+        }
 
     }
-
-
-
-
 }
 
 ?>
--- lib/response.php
+++ lib/response.php
@@ -1,16 +1,49 @@
 <?php
 
+/**
+ * response class for YAcURLWrapper
+ * @author vijayant
+ * @usage holds the response data once the bot has finished executing
+ * @TODO format output and add cookie saving
+ **/
+
 namespace bot {
 
     class response {
-        private $status;
-        private $content;
+        private $content = null;
+        private $info = array();
 
+        public function setInfo($info){
+            $this->info = $info;
+        }
+
+        public function setContent($content){
+            $this->content = $content;
+        }
+
+        public function getContent(){
+            return $this->content;
+        }
+
+        public function getStatusCode(){
+            if(isset($this->info["http_code"]))
+                return $this->info["http_code"];
+            else
+                return null;
+        }
+
+        public function getInfo($name){
+            if(isset($this->info[$name]))
+                return $this->info[$name];
+            else
+                return null;
+        }
+
+        public function debug(){
+            print_r($this->info);
+            echo "\ncontent-length = " . strlen($this->content);
+        }
     }
-
-
-
-
 }
 
 ?>
